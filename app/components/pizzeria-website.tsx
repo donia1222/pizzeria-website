0c2761d47221e565247d692c7047d7c33c032465
lwev <106478077+lwev@users.noreply.github.com>
2025-03-22 13:00:57 +0100

    “update02”

--- app/components/pizzeria-website.tsx
+++ app/components/pizzeria-website.tsx
@@ -191,8 +191,8 @@ const testimonials = [
 
 // Gallery images
 const galleryImages = [
-  "https://images.unsplash.com/photo-1564936281291-294551497d81?q=80&w=300&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?q=80&w=300&auto=format&fit=crop",
+  "/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_2829-19739.jpg",
+  "/table-arrangement-luxury-restaurant_23-2150598334.jpg",
   "https://images.unsplash.com/photo-1590947132387-155cc02f3212?q=80&w=300&auto=format&fit=crop",
   "https://images.unsplash.com/photo-1593504049359-74330189a345?q=80&w=300&auto=format&fit=crop",
   "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=300&auto=format&fit=crop",

85ab8e5ece4cf5359e383295d1c7efc8a8db8f5f
lwev <106478077+lwev@users.noreply.github.com>
2025-05-15 00:59:28 +0200

    “meta”

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,17 @@
 import PizzeriaWebsite from "../components/pizzeria-website"
+import type { MetaFunction } from "@remix-run/node"
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Bouquet Mediterraneo" },
+    { name: "description", content: "Cucina Mediterranea & Pizza" },
+    { name: "theme-color", content: "#000000" },
+    { name: "keywords", content: "pizzeria, sevelen" },
+    { property: "og:title", content: "Bouquet Mediterraneo" },
+    { property: "og:description", content: "Cucina Mediterranea & Pizza" },
+    
+  ]
+}
 
 export default function Home() {
   return <PizzeriaWebsite />
